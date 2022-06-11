b7b90f3b40241a5ec70d36aba19e49145851a811
Bobby Lee <superbahbi@users.noreply.github.com>
2022-06-11 07:12:39 -0700

    added click sound

--- server/app.ts
+++ server/app.ts
@@ -12,17 +12,34 @@ const io = socketIo(server, {
 });
 var rooms = [];
 var gameState = []
+const winState = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ]
+
 io.on("connection", socket => {
     
-    console.log(socket.id + ' ==== connected');
+    console.log(chalk.green(socket.id + ' ==== connected'));
     socket.on('create', function(id) {
         socket.join(id);
-        rooms.push({room: id, players: [socket.id], gameState: ["", "", "", "", "", "", "", "", ""], turn: "x"});
+        rooms.push({
+            room: id, 
+            players: [socket.id], 
+            gameState: ["", "", "", "", "", "", "", "", ""], 
+            turn: "x",
+            winner: ""
+        });
         console.log(chalk.green(socket.id + ' ==== created room ' + id));
         rooms.forEach(r => { 
             if (r.room === id) {
                 // Sending the rooms to the client
-                socket.emit('onMessage', r);
+                socket.emit('updateBoard', r);
             }
         })
     });
@@ -32,29 +49,36 @@ io.on("connection", socket => {
             if (r.room === id) {
                 r.players.push(socket.id);
                 // Sending the rooms to the client
-                socket.emit('onMessage', r);
+                socket.emit('updateBoard', r);
                 Array.from(socket.rooms)
                 .filter(it => it !== socket.id)
                 .forEach(id => {
-                    socket.to(id).emit('onMessage', r, function (err, success) {
+                    socket.to(id).emit('updateBoard', r, function (err, success) {
                     });
                 });
             }
         })
         console.log(chalk.green(socket.id + ' ==== joined room ' + id));   
     });
-    socket.on("emitMessage", message => {
+    socket.on("lobbyData", message => {
         Array.from(socket.rooms)
             .filter(it => it !== socket.id)
             .forEach(id => {
-                socket.to(id).emit('onMessage', message, function (err, success) {
-                    console.log(message)
-                    // gameState.push(message)  
+                socket.to(id).emit('updateBoard', message, function (err, success) {
+                    winState.forEach(state => {
+                        if(state.every(i => (message?.gameState[i]) === "x")){
+                            message.winner = "x";
+                        }
+                        else if(state.every(i => (message?.gameState[i]) === "o")){
+                            message.winner = "o";
+                        }
+                    })
+                    io.in(id).emit('updateBoard', message)
                 });
             });
     });
     socket.on("disconnect", () => {
-        console.log(socket.id + ' ==== diconnected');
+        console.log(chalk.red(socket.id + ' ==== diconnected'));
         socket.removeAllListeners();
     });
 });
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect } from 'react';
 import socketIOClient from 'socket.io-client';
 import { 
   useClipboard, 
@@ -7,60 +7,52 @@ import {
 
 } from '@chakra-ui/react'
 import './App.css';
+import useSound from 'use-sound';
 
+
+const popUpOff = require("../src/sounds/pop-up-off.mp3");
 const ENDPOINT: any = "http://localhost:3001";
 const defaultBoard = ["", "", "", "", "", "", "", "", ""];
-const winState = [
-  [0, 1, 2],
-  [3, 4, 5],
-  [6, 7, 8],
-  [0, 3, 6],
-  [1, 4, 7],
-  [2, 5, 8],
-  [0, 4, 8],
-  [2, 4, 6]
-]
+
 type lobbyType = {
   room: string,
   players: Array<string>,
   gameState: typeof defaultBoard,
   turn: string,
+  winner: string,
 }
 
 const App : React.FC =()=> {
-  const inputRef = useRef<HTMLInputElement | any>(null);
   const [socket, setSocket] = React.useState<any>(null);
   const [turn, setTurn] = React.useState("x");
-  const [winner, setWinner] = React.useState("");
   const [room, setRoom] = React.useState<string>("");
   const [roomInput, setRoomInput] = React.useState("");
   const [playerMove, setPlayerMove] = React.useState("");
   const [lobby, setLobby] = React.useState<lobbyType>();
   const { hasCopied, onCopy } = useClipboard(room)
+  const [toggle, setToggle] = React.useState(false)
+
+  const [PlaySound] = useSound(popUpOff, { id: 'off', volume: 0.25 });
 
   const onHandleClick = (e: React.MouseEvent<HTMLButtonElement>) => {
     let id = e.currentTarget.id;
-    console.log(lobby)
+    setToggle(!toggle)
     if (lobby?.turn === playerMove && lobby?.players.length === 2) {
-      
+      PlaySound();
       let newBoard = [...lobby.gameState];
-      if (newBoard[parseInt(id)] === "" && winner === "") {
+      if (newBoard[parseInt(id)] === "" && lobby?.winner === "") {
         newBoard[parseInt(id)] = lobby.turn;
       }
       lobby["gameState"] = newBoard;
       lobby["turn"] = playerMove === "x" ? "o" : "x";
       setTurn(playerMove === "x" ? "o" : "x");
-      socket.emit("emitMessage", lobby);
+      socket.emit("lobbyData", lobby);
+      
     }
   };
 
   const onSocketMakeRoom = () => {
-    
     const id = Math.random().toString(36).substr(2, 9);
-    console.log(inputRef.current)
-    if (inputRef.current) {
-      inputRef.current = id;
-    }
     setRoom(id)
     setPlayerMove("x")
     setTurn("x")
@@ -76,42 +68,38 @@ const App : React.FC =()=> {
   useEffect((): (() => void) => {
     const newSocket = socketIOClient(ENDPOINT);
     setSocket(newSocket);
-    newSocket.on("onMessage", data => {
+    newSocket.on("updateBoard", data => {
       setLobby(data)
       setTurn(data.turn);
     });
     return () => newSocket.disconnect();
   }, []);
 
-  useEffect(() => {
-    winState.forEach(state => {
-      state.every(i => lobby?.gameState[i] === "x") && setWinner("x");
-    })
-    winState.forEach(state => {
-      state.every(i => lobby?.gameState[i] === "o") && setWinner("o");
-    })
-  }, [lobby, turn]);
   return (
     <div className="app">
       <h1 className="title">Tic Tac Toe MMO</h1>
-      <h3>{winner && `Player ${winner} wins`}</h3>
+      <h3>{lobby?.winner && `Player ${lobby.winner} wins`}</h3>
       <p> {room && `Player ${turn.toUpperCase()} turn`}</p>
       <p>{room && `You are playing as ${playerMove}`}</p>
-      <p>{`Lobby ${lobby?.players ? lobby?.players.length : 0  }/2`}</p>
+      <p>{room && `Lobby ${lobby?.players ? lobby?.players.length : 0  }/2`}</p>
     {room && <div className="board">
       {lobby?.gameState.map((row, index) => {
-        return <button className="square" disabled={false} key={index} onClick={onHandleClick} id={index.toString()}>
-          <span className="letter">
+        return <button
+          id={index.toString()}
+          className="square" 
+          disabled={false} 
+          key={index} 
+          onClick={onHandleClick} 
+          >
+          {row && <span className="letter">
             {row}
-          </span>
+          </span>}
         </button>
       })}
     </div>}
-     
-
       <div className="input">
         <>
-          {room  ? 
+          {room ? 
           <>
           <div>
             <Input value={room} isReadOnly />
@@ -130,8 +118,6 @@ const App : React.FC =()=> {
           </div>
           </>
           }
-        
-     
         </>
       <div>
         <Button onClick={onSocketMakeRoom}>Make a room</Button>
